f6bbd991805add22c6d842c2760ecbeeea7bbd66
Adam Casey <adam.jw.casey@gmail.com>
2023-01-22 11:41:35 -0700

    Rework API to bring count_visible inside of TreeGrid

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,37 @@
-/// Counts and returns the number of trees visible from outside
-/// # Examples
-/// ```
-/// use advent_of_code_2022_08::count_visible;
-/// assert_eq!(21, count_visible(
-///     concat!(
-///         "30373\n",
-///         "25512\n",
-///         "65332\n",
-///         "33549\n",
-///         "35390"
-/// )));
-/// ```
-pub fn count_visible(input: &str) -> u32{
-    todo!();
+pub struct TreeGrid{
+    rows: Vec<Vec<u32>>
 }
+
+impl TreeGrid{
+    pub fn new(input: &str) -> Option<Self>{
+        let new = Self{
+            rows: input.split('\n').map(|row| row.chars().map(|c| c.to_digit(10).unwrap()).collect()).collect()
+        };
+
+        // Check that all rows are same length to ensure it is a proper grid
+        if new.rows.windows(2).all(|w| w[0] == w[1]){
+            return Some(new);
+        }else{
+            return None;
+        }
+    }
+
+    /// Counts and returns the number of trees visible from outside
+    /// # Examples
+    /// ```
+    /// use advent_of_code_2022_08::TreeGrid;
+    /// let tg = TreeGrid::new(
+    ///     concat!(
+    ///         "30373\n",
+    ///         "25512\n",
+    ///         "65332\n",
+    ///         "33549\n",
+    ///         "35390"
+    /// )).unwrap();
+    /// assert_eq!(tg.count_visible(), 21);
+    /// ```
+    pub fn count_visible(&self) -> u32{
+        todo!();
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use advent_of_code_2022_08::count_visible;
+use advent_of_code_2022_08::TreeGrid;
 use std::env;
 use std::fs;
 
@@ -6,6 +6,7 @@ fn main() {
     let args = env::args().collect::<Vec<_>>();
     let file_path = &args[1];
     let contents = fs::read_to_string(file_path).expect("Should have been able to read {file_path}");
+    let tg = TreeGrid::new(&contents);
 
-    println!("There are {} trees visible.", count_visible(&contents));
+    println!("There are {} trees visible.", tg.unwrap().count_visible());
 }

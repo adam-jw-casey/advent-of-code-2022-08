16a0ceb9093f192cbd102e5aa570b75e4aece8ad
Adam Casey <adam.jw.casey@gmail.com>
2023-01-22 10:43:56 -0700

    Add skeleton with failing test

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+/// Counts and returns the number of trees visible from outside
+/// # Examples
+/// ```
+/// use advent_of_code_2022_08::count_visible;
+/// assert_eq!(21, count_visible(
+///     concat!(
+///         "30373\n",
+///         "25512\n",
+///         "65332\n",
+///         "33549\n",
+///         "35390"
+/// )));
+/// ```
+pub fn count_visible(input: &str) -> u32{
+    todo!();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use advent_of_code_2022_08::count_visible;
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<_>>();
+    let file_path = &args[1];
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read {file_path}");
+
+    println!("There are {} trees visible.", count_visible(&contents));
 }
